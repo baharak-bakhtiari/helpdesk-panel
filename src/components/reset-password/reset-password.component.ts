c2e17671e4f35ef8b6d3c3e341c3297a85a7bbb5
Baharak Bakhtiari <baharakbakhtiyari@gmail.com>
2024-01-30 09:09:55 +0330

    edit reset password structure

--- src/components/reset-password/reset-password.component.ts
+++ src/components/reset-password/reset-password.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { InputPasswordComponent } from '../input-password/input-password.component';
 
 @Component({
   selector: 'reset-password',
   standalone: true,
-  imports: [RouterLink, InputPasswordComponent, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, RouterLink, InputPasswordComponent, FormsModule, ReactiveFormsModule],
   templateUrl: './reset-password.component.html',
   styleUrl: './reset-password.component.scss'
 })
@@ -16,10 +17,10 @@ export class ResetPasswordComponent {
 
   constructor(formBuilder: FormBuilder) {
     this.form = formBuilder.group({
-      otpCode: [""],
-      newPassword: [""],
-      RepeatNewPassword: [""],
-      captchaCode: [""]
+      otpCode: [null, Validators.required],
+      newPassword: [null, Validators.required],
+      repeatNewPassword: [null, Validators.required],
+      captchaCode: [null, Validators.required]
     });
   }
 
