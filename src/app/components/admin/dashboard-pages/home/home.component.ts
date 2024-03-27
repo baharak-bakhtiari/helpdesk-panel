39c0196b323c64e77a5132d66962f2f21922934d
Baharak Bakhtiari <baharakbakhtiyari@gmail.com>
2024-03-27 13:02:58 +0330

    create tickets overview table

--- src/app/components/admin/dashboard-pages/home/home.component.ts
+++ src/app/components/admin/dashboard-pages/home/home.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { ColDef } from 'ag-grid-community';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [],
+  imports: [AgGridAngular],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
 
+  // Row Data: The data to be displayed.
+  rowData = [
+    { "شناسه تیکت": "2513", "موضوع": "دسترسی به پورتفو", "درخواست‌کننده": "علی علوی", "تاریخ ایجاد": "23/10/1402", "اولویت": "متوسط", "وضعیت": "باز", "بازخورد": "عالی", "عملیات" :"ستاره/ اصلاح"},
+  ];
+
+  // Column Definitions: Defines the columns to be displayed.
+  colDefs: ColDef[] = [
+    { field: "شناسه تیکت" },
+    { field: "موضوع" },
+    { field: "درخواست‌کننده" },
+    { field: "تاریخ ایجاد" },
+    { field: "اولویت" },
+    { field: "وضعیت" },
+    { field: "بازخورد" },
+    { field: "عملیات" },
+  ];
 }
